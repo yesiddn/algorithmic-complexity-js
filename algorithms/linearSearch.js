e440ec397c2c8f2eb3876a1a9661df82d1cc9423
Yesid <yesidrodriguez305@gmail.com>
2024-02-02 16:47:58 -0500

    Add evaluation of time complexity with Big-O notation

---
+++ algorithms/linearSearch.js
@@ -0,0 +1,13 @@
+/**
+ * Complejidad Temporal -> O( n + 3 ) -> O( n )
+ * Complejidad Espacial -> O(  )
+ * Espacio Auxiliar -> O(  )
+ */
+function linearSearch(arreglo, clave) {
+  for (let indice = 0; indice < arreglo.length; indice++) { // O(n)
+    if (arreglo[indice] === clave) { // O(1)
+      return indice; // O(1)
+    }
+  }
+  return -1; // O(1)
+}
