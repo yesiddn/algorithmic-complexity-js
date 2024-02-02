703c9fcb5945c9169e6e6795153fd8705a508a1d
Yesid <yesidrodriguez305@gmail.com>
2024-02-01 20:47:37 -0500

    Calculate the Big-O notation for time algorithmic complexity

---
+++ performance/time/big-o.js
@@ -0,0 +1,6 @@
+// Calculate the Big-O notation for time algorithmic complexity
+let bar = 'test' // O(1)
+if () { } // O(1)
+for () { } // O(n)
+while () { } // O(n)
+for () { for () { } } // O(n^2)
