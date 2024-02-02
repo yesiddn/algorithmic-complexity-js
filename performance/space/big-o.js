b2c72778695e1d319d5108a9d25baa307fc491e0
Yesid <yesidrodriguez305@gmail.com>
2024-02-01 20:48:36 -0500

    Calculate the Big-O notation for space algorithmic complexity

---
+++ performance/space/big-o.js
@@ -0,0 +1,6 @@
+// Calculate the Big-O notation for space algorithmic complexity
+let bar = 'test' // 0(1)
+if () { } // 0(1)
+for () { } // 0(1)
+let resultado = [1, 2, ..., n] // 0(n)
+let dimensional = [[2, 4], [6, 8], [10, 12]] //0(n^2)
