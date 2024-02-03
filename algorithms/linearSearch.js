2a6a73e0c6c232d9e589f1bbcc7e91e8f751df1d
Yesid <yesidrodriguez305@gmail.com>
2024-02-02 19:09:05 -0500

    Add evaluation of space complexity with Big-O notation

--- algorithms/linearSearch.js
+++ algorithms/linearSearch.js
@@ -1,13 +1,13 @@
 /**
  * Complejidad Temporal -> O( n + 3 ) -> O( n )
- * Complejidad Espacial -> O(  )
- * Espacio Auxiliar -> O(  )
+ * Complejidad Espacial -> O( n + 1 ) -> O( n )
+ * Espacio Auxiliar -> Complejidad Espacial - Espacio de Entrada -> O( n ) - O( 1 ) -> O( 1 )
  */
-function linearSearch(arreglo, clave) {
-  for (let indice = 0; indice < arreglo.length; indice++) { // O(n)
-    if (arreglo[indice] === clave) { // O(1)
-      return indice; // O(1)
+function linearSearch(arreglo, clave) { // O(n)
+  for (let indice = 0; indice < arreglo.length; indice++) { // O(1)
+    if (arreglo[indice] === clave) {
+      return indice;
     }
   }
-  return -1; // O(1)
+  return -1;
 }
